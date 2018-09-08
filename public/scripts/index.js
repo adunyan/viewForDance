fbef8302fce0d15b06ddd2828987206f3b838929
Air <honoonokitune@live.jp>
2018-09-08 17:25:38 +0900

    再生、一時停止、戻る、進むの実装

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -22,13 +22,62 @@ window.onload = () => {
         } else {
             for (var i = 0; i < files.length; i++) {
                 let video = document.createElement("video");
+
                 video.src = window.URL.createObjectURL(files[i]);
-                
                 video.onload = function (e) {
+                    console.log("video URL revoked");
                     window.URL.revokeObjectURL(this.src);
                 }
-
+                video.load();
+                
                 viewer.appendChild(video);
+                
+                const prev = document.getElementById("prev");
+                const next = document.getElementById("next");
+                const play = document.getElementById("play");
+
+                let rate = 1.0;
+                let shiftValue = 5;
+                prev.addEventListener("click", (e) => {
+                    e.preventDefault();
+                    console.log("prev clicked");
+                    if (video) {
+                        video.currentTime -= shiftValue;
+                    }
+                });
+                next.addEventListener("click", (e) => {
+                    e.preventDefault();
+                    console.log("prev clicked");
+                    if (video) {
+                        video.currentTime += shiftValue;
+                    }
+                });
+
+                play.addEventListener("click", (e) => {
+                    e.preventDefault();
+                    console.log("play clicked");
+
+                    if (video) {
+
+                        if(video.played == 0) {
+                            console.log("video started");
+
+                            video.play();
+                        } else {
+                            if(!video.paused){
+                                console.log("video paused");
+
+                                video.pause();
+                            } else {
+                                console.log("video is playing");
+                                video.play();
+                            }
+                        }
+                    }
+                });
+                
+
+
 
             }
         }
